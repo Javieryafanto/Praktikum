22f0da8a576e1d55548cdafac75c272a59d29fe8
Javier Yafanto Joedo <javieryafanto0306@gmail.com>
2023-03-10 10:58:01 +0700

    v1.1

--- 10 maret 23.py
+++ 10 maret 23.py
@@ -3,3 +3,7 @@ user = [
     {'nama': 'sherlock holmes', 'nik': '4321', 'jenis_kelamin': 'male', 'tanggal_lahir': '1876-08-16'},
     {'nama': 'irene adler', 'nik': '6789', 'jenis_kelamin': 'female', 'tanggal_lahir': '1884-10-07'}
 ]
+nama = input("Masukkan Nama: ")
+nik = input("Masukkan NIK: ")
+jenis_kelamin = input("Masukkan Jenis Kelamin:(ex : Pria/Wanita) ")
+tanggal_lahir = input("Masukkan tanggal lahir (ex : 2020-01-02): ")
